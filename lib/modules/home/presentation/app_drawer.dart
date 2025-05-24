aea1b9be40c0ef5966c28ef079fae52c77d958e4
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-05-24 22:18:33 +0300

    Add lang settings to drawer

--- lib/modules/home/presentation/app_drawer.dart
+++ lib/modules/home/presentation/app_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sight_mate/modules/shared/i18n/i18n.dart';
 import 'package:sight_mate/modules/shared/theme/theme.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -13,18 +14,27 @@ class AppDrawer extends StatelessWidget {
           children: [
             Container(
               padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
-              alignment: Alignment.centerLeft,
+              alignment: Alignment.center,
               child: Text(
-                'App Menu',
+                L10n.current.appMenu,
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
             const Divider(),
             ListTile(
               leading: const Icon(Icons.color_lens),
-              title: const Text('Theme Settings'),
+              title: Text(L10n.current.themeSettings),
               onTap: () {
                 Navigator.of(context).popAndPushNamed(ThemeSettingsRoute.name);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.language),
+              title: Text(L10n.current.languageSettings),
+              onTap: () {
+                Navigator.of(
+                  context,
+                ).popAndPushNamed(LanguageSettingsRoute.name);
               },
             ),
           ],
