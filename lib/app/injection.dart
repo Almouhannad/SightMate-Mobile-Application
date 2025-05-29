00d2ff88d70798cbc205831e97fa942f0a0fd075
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-05-30 02:28:28 +0300

    Register DI for offline ocr

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sight_mate/modules/ocr/data/ocr_data.dart';
+import 'package:sight_mate/modules/ocr/domain/providers/ocr_provider.dart';
 import 'package:sight_mate/modules/shared/theme/theme.dart';
 import 'package:sight_mate/modules/shared/i18n/i18n.dart';
 
@@ -23,5 +25,17 @@ Future<void> configureDependencies() async {
   // I18nNotifier: depends on I18nRepository
   getIt.registerLazySingleton<I18nNotifier>(
     () => I18nNotifier(getIt<I18nRepository>()),
+  );
+
+  // OCR
+  void disposeOcrProvider(OcrProvider provider) {
+    provider.dispose();
+  }
+
+  // Offline
+  getIt.registerLazySingleton<OcrProvider>(
+    () => OcrProviderOfflineImpl(),
+    instanceName: 'offline',
+    dispose: disposeOcrProvider,
   );
 }
