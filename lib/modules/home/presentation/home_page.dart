3eb2ba349d62b2fd4e0a5223cb895151f3364b42
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-05-29 13:18:25 +0300

    Modify home page to handle feature screens

--- lib/modules/home/presentation/home_page.dart
+++ lib/modules/home/presentation/home_page.dart
@@ -7,10 +7,47 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const AppDrawer(),
-      appBar: AppBar(title: Text(L10n.of(context).appName)),
-      body: HelloWorldWidget(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        drawer: const AppDrawer(),
+        appBar: AppBar(
+          title: Text(L10n.of(context).appName),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: [
+              const TabBarItem(text: 'Home'),
+              const TabBarItem(text: 'Live Text Recognition'),
+              const TabBarItem(text: 'Crop and Recognize Text'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            HelloWorldWidget(),
+            HelloWorldWidget(),
+            HelloWorldWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TabBarItem extends StatelessWidget {
+  final String text;
+  const TabBarItem({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textStyle = theme.textTheme.labelLarge!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Tab(
+      child: Row(
+        children: [Text(text, style: textStyle), const SizedBox(width: 6)],
+      ),
     );
   }
 }
