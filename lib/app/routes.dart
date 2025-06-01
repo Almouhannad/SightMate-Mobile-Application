c476c00249fc8b8a7300e2476c9f9d16d6179bc7
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-01 23:40:18 +0300

    Update routes

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sight_mate/modules/ocr/presentation/ocr_presentation.dart';
 import 'package:sight_mate/modules/shared/i18n/i18n.dart';
 import 'package:sight_mate/modules/shared/theme/theme.dart';
 
 Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-  switch (settings.name) {
-    case ThemeSettingsRoute.name:
-      return MaterialPageRoute(
-        builder: (_) => const ThemeSettings(),
-        settings: settings,
-      );
-    case LanguageSettingsRoute.name:
-      return MaterialPageRoute(
-        builder: (_) => const LanguageSettings(),
-        settings: settings,
-      );
-    default:
-      return null;
+  Map<String, Widget> routes = {};
+  routes[ThemeSettingsRoute().link] = ThemeSettings();
+  routes[LanguageSettingsRoute().link] = LanguageSettings();
+  routes[OcrHomeScreenRoute().link] = OcrHomeScreen();
+
+  if (routes.keys.contains(settings.name)) {
+    return MaterialPageRoute(
+      builder: (_) => routes[settings.name]!,
+      settings: settings,
+    );
   }
+  return null;
 }
