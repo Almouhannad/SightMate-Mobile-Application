28694516f468649783a2b8216bf92960e03733ef
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-01 23:40:05 +0300

    Change routes definition

--- lib/modules/shared/i18n/presentation/language_settings_route.dart
+++ lib/modules/shared/i18n/presentation/language_settings_route.dart
@@ -1,3 +1,10 @@
-class LanguageSettingsRoute {
-  static const String name = '/settings/language';
+import 'package:sight_mate/core/page_route_settings.dart';
+import 'package:sight_mate/modules/shared/i18n/i18n.dart';
+
+class LanguageSettingsRoute extends PageRouteSettings {
+  @override
+  String get link => '/settings/language';
+
+  @override
+  String get name => L10n.current.languageSettings;
 }
