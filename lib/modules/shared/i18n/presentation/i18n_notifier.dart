90fcd4c9db2d8287ce674f4c814cf92e95c612fe
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-05 22:54:17 +0300

    Add restart app after language change

--- lib/modules/shared/i18n/presentation/i18n_notifier.dart
+++ lib/modules/shared/i18n/presentation/i18n_notifier.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:restart_app/restart_app.dart';
+import 'package:sight_mate/app/injection.dart';
 import 'package:sight_mate/modules/shared/i18n/i18n.dart';
+import 'package:sight_mate/modules/shared/tts/domain/tts_provider.dart';
 
 /// A ChangeNotifier that holds the current Locale and notifies listeners on changes
 class I18nNotifier extends ChangeNotifier {
@@ -8,8 +11,10 @@ class I18nNotifier extends ChangeNotifier {
   Locale? _locale;
   bool _initialized = false;
 
-  I18nNotifier(this._repository) {
-    _loadInitialLocale();
+  I18nNotifier(this._repository);
+
+  Future<void> initilize() async {
+    await _loadInitialLocale();
   }
 
   /// The current locale exposed to the UI
@@ -35,6 +40,7 @@ class I18nNotifier extends ChangeNotifier {
   Future<void> updateLocale(Locale newLocale) async {
     if (newLocale == _locale) return;
     _locale = newLocale;
+    Restart.restartApp();
     notifyListeners();
     await _repository.saveLocale(newLocale);
   }
