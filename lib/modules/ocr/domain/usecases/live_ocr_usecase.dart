7c05151d97d6f63acc962ce2b2333332c87ed84f
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-05 23:21:19 +0300

    add live ocr usecase

---
+++ lib/modules/ocr/domain/usecases/live_ocr_usecase.dart
@@ -0,0 +1,51 @@
+import 'package:get_it/get_it.dart';
+import 'package:sight_mate/modules/ocr/domain/ocr_domain.dart';
+
+class LiveOcrUsecaseConfig {
+  static final Duration frameInterval = Duration(milliseconds: 1000);
+  static final Duration repeatInterval = Duration(seconds: 10);
+  static final double confidenceThreshold = 0.2;
+  static final int maximumLength = 50;
+}
+
+class LiveOcrUsecase {
+  final Map<String, DateTime> _lastDetectedIn = {};
+  late OcrProvider _ocrProvider;
+
+  Duration get frameInterval => LiveOcrUsecaseConfig.frameInterval;
+
+  Future<String> processFrameBytes(List<int> bytes) async {
+    _ocrProvider = GetIt.I.get<OcrProvider>(instanceName: 'offline');
+
+    final results = await _ocrProvider.processImage(OcrInput(bytes: bytes));
+
+    String textToSpeak = '';
+    final DateTime now = DateTime.now();
+    for (var result in results) {
+      if (shouldSpeak(result, now)) {
+        textToSpeak += '${result.text}\n';
+        _lastDetectedIn[result.text.toLowerCase()] = now;
+      }
+    }
+    return textToSpeak;
+  }
+
+  bool shouldSpeak(OcrResult ocrResult, DateTime now) {
+    bool result = true;
+
+    // Last sopken time
+    final lastSpokenTime = _lastDetectedIn[ocrResult.text.toLowerCase()];
+    result &=
+        (lastSpokenTime == null ||
+            now.difference(lastSpokenTime) >=
+                LiveOcrUsecaseConfig.repeatInterval);
+
+    final confidence = ocrResult.confidence ?? 0.0;
+    result &= confidence >= LiveOcrUsecaseConfig.confidenceThreshold;
+
+    final textLength = ocrResult.text.length;
+    result &= textLength <= LiveOcrUsecaseConfig.maximumLength;
+
+    return result;
+  }
+}
