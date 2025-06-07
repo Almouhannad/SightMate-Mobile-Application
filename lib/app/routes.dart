c11d3f1d095e7c1e779383e4ec9cc7748ed2c699
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-06-07 17:32:00 +0300

    Register new feature in routes and drawer

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:sight_mate/modules/ocr/presentation/ocr_presentation.dart';
 import 'package:sight_mate/modules/shared/i18n/i18n.dart';
 import 'package:sight_mate/modules/shared/theme/theme.dart';
+import 'package:sight_mate/modules/yolo_object_recognition/yolo_object_recognition.dart';
 
 Route<dynamic>? onGenerateRoute(RouteSettings settings) {
   Map<String, Widget> routes = {};
   routes[ThemeSettingsRoute().link] = ThemeSettings();
   routes[LanguageSettingsRoute().link] = LanguageSettings();
   routes[OcrHomeScreenRoute().link] = OcrHomeScreen();
+  routes[ObjectRecognitionHomeScreenRoute().link] =
+      ObjectRecognitionHomeScreen();
 
   if (routes.keys.contains(settings.name)) {
     return MaterialPageRoute(
--- lib/modules/yolo_object_recognition/yolo_object_recognition.dart
+++ lib/modules/yolo_object_recognition/yolo_object_recognition.dart
@@ -4,3 +4,4 @@ export 'config/yolo_object_recognition_config.dart';
 export 'controllers/yolo_controller.dart';
 export 'widgets/yolo_view_widget.dart';
 export 'screens/object_recognition_home_screen.dart';
+export 'object_recognition_home_screen_route.dart';
