cf9d768403488bebc69ab3d3a9b597f05ddef16a
Almouhannad Hafez <almouhannad.hafez@gmail.com>
2025-07-04 21:23:00 +0300

    Add 400 bad requests interceptor

--- lib/modules/shared/api_client/api_client.dart
+++ lib/modules/shared/api_client/api_client.dart
@@ -9,7 +9,12 @@ class ApiClient {
 
   ApiClient._internal() {
     dio = Dio(BaseOptions(baseUrl: Config.backendApiBaseUri));
+
+    // 1) attach auth interceptor
     dio.interceptors.add(AuthInterceptor());
+
+    // 2) attach bad-request filter interceptor
+    dio.interceptors.add(BadRequestInterceptor());
   }
 
   Future<Response> get(String path, {Map<String, dynamic>? query}) {
@@ -34,5 +39,36 @@ class AuthInterceptor extends Interceptor {
       options.headers['Authorization'] = 'Bearer $token';
     }
     handler.next(options);
+  }
+}
+
+class BadRequestInterceptor extends Interceptor {
+  final _validationErrorsProperty = Config.serverValidationErrorsPropertyName;
+  final _validationErrorDescriptionProperty =
+      Config.serverValidationErrorDescriptionPropertyName;
+  final _clientValidationErrorsProperty =
+      Config.clientValidationErrorsPropertyName;
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final response = err.response;
+    if (response != null && response.statusCode == 400) {
+      List<String> validationErrors = [];
+      for (var validationError in response.data[_validationErrorsProperty]) {
+        validationErrors.add(
+          validationError[_validationErrorDescriptionProperty],
+        );
+      }
+      handler.resolve(
+        Response(
+          requestOptions: err.requestOptions,
+          statusCode: 400,
+          data: {_clientValidationErrorsProperty: validationErrors},
+        ),
+      );
+    } else {
+      // Not 400, let it go
+      handler.next(err);
+    }
   }
 }
